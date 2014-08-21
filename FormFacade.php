bad94f257448c3a752cfac526fab28903818038d
Taylor Otwell <taylorotwell@gmail.com>
2014-08-21 12:51:27 -0500

    working on 4.3 release of stand-alone component.

---
+++ FormFacade.php
@@ -0,0 +1,15 @@
+<?php namespace Illuminate\Html;
+
+/**
+ * @see \Illuminate\Html\FormBuilder
+ */
+class FormFacade extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'form'; }
+
+}
---
+++ HtmlFacade.php
@@ -0,0 +1,15 @@
+<?php namespace Illuminate\Html;
+
+/**
+ * @see \Illuminate\Html\HtmlBuilder
+ */
+class HtmlFacade extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'html'; }
+
+}
