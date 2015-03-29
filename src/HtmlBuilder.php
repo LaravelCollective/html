6b023d8055870e5d4dcc9dccb1f30a55b7bc589a
Joseph Silber <contact@josephsilber.com>
2015-03-29 18:06:56 -0400

    Move comment to correct spot

--- src/HtmlBuilder.php
+++ src/HtmlBuilder.php
@@ -339,9 +339,6 @@ class HtmlBuilder {
 	{
 		$html = array();
 
-		// For numeric keys we will assume that the key and the value are the same
-		// as this will convert HTML attributes such as "required" to a correct
-		// form like required="required" instead of using incorrect numerics.
 		foreach ((array) $attributes as $key => $value)
 		{
 			$element = $this->attributeElement($key, $value);
@@ -361,6 +358,9 @@ class HtmlBuilder {
 	 */
 	protected function attributeElement($key, $value)
 	{
+		// For numeric keys we will assume that the key and the value are the same
+		// as this will convert HTML attributes such as "required" to a correct
+		// form like required="required" instead of using incorrect numerics.
 		if (is_numeric($key)) $key = $value;
 
 		if ( ! is_null($value)) return $key.'="'.e($value).'"';
