a3f4a498e921adc3d7842a72f71c033481391d38
Ryan Hayle <hackel@users.noreply.github.com>
2016-03-29 16:26:54 -0500

    Update Form::date() to output values in RFC 3339 format
    The HTML5 input type=date element requires the value to be in RFC 3339 format.  It's currently being output as `Y-m-d`, but RFC 3339 requires a complete date *and* time.
    
    See: 
    http://w3c.github.io/html-reference/input.date.html#input.date.attrs.value
    http://tools.ietf.org/html/rfc3339#section-5.6

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -367,7 +367,7 @@ class FormBuilder
     public function date($name, $value = null, $options = [])
     {
         if ($value instanceof DateTime) {
-            $value = $value->format('Y-m-d');
+            $value = $value->format(DateTime::RFC3339);
         }
 
         return $this->input('date', $name, $value, $options);
