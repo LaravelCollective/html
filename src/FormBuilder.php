4aae8fb4384f9aaa17df44ad843b45af9cd721b3
mlantz <mattlantz@gmail.com>
2017-05-19 04:15:59 -0400

    Fixes for #337 and #259

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -739,6 +739,8 @@ class FormBuilder
     {
         if (is_array($selected)) {
             return in_array($value, $selected) ? 'selected' : null;
+        } elseif ($selected instanceof Collection) {
+            return $selected->contains($value) ? 'selected' : null;
         }
 
         return ((string) $value == (string) $selected) ? 'selected' : null;
@@ -1128,6 +1130,7 @@ class FormBuilder
 
             if ($hasNullMiddleware
                 && is_null($old)
+                && is_null($value)
                 && !is_null($this->view->shared('errors'))
                 && count($this->view->shared('errors')) > 0
             ) {
