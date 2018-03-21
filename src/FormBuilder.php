57a59ed277119b162d8e40c97c3b78e996423da1
Tortue Torche <tortuetorche@users.noreply.github.com>
2018-03-21 17:20:00 +0100

    [5.5] Fix form method of hidden input '_method' is preserved on next request
    This fix was merged in the pull request https://github.com/LaravelCollective/html/pull/460
    But the pull request https://github.com/LaravelCollective/html/pull/471 accidentally revert this fix

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -1183,7 +1183,7 @@ class FormBuilder
         }
 
         $request = $this->request($name);
-        if (! is_null($request)) {
+        if (! is_null($request) && $name !== '_method') {
             return $request;
         }
 
