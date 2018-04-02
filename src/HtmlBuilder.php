be30f3816404ade02ccf9ec8b4d05e83385451d9
Adrian Crisan <adrian.crisan88@gmail.com>
2018-04-02 17:23:13 +0300

    Fix the issue where defaults has a higher priority then the attributes

--- src/HtmlBuilder.php
+++ src/HtmlBuilder.php
@@ -94,7 +94,7 @@ class HtmlBuilder
     {
         $defaults = ['media' => 'all', 'type' => 'text/css', 'rel' => 'stylesheet'];
 
-        $attributes = array_merge($attributes, $defaults);
+        $attributes = array_merge($defaults, $attributes);
 
         $attributes['href'] = $this->url->asset($url, $secure);
 
